67c84fd23cc21050e5d773ecc198d5c6fc0260f3
Timboy67678 <me@timboy67678.com>
2026-07-10 22:42:24 +1000

    feat: initialize RuneLite plugin project and remove deprecated web configurations

---
+++ runelite-plugin/src/test/java/com/osrsai/OsrsAiPanelTest.java
@@ -0,0 +1,26 @@
+package com.osrsai;
+
+import java.lang.reflect.Method;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class OsrsAiPanelTest
+{
+    @Test
+    public void formatsHeadingsListsAndEscaping() throws Exception
+    {
+        OsrsAiPanel panel = new OsrsAiPanel(null);
+        Method formatter = OsrsAiPanel.class.getDeclaredMethod("formatMarkdownToHtml", String.class);
+        formatter.setAccessible(true);
+
+        String input = "### Player Profile\n- Name: tim\n- **Combat**: 91\n\nSafe <tag>";
+        String html = (String) formatter.invoke(panel, input);
+
+        Assert.assertTrue(html.contains("font-weight:bold"));
+        Assert.assertTrue(html.contains("<ul"));
+        Assert.assertTrue(html.contains("<li>Name: tim</li>"));
+        Assert.assertTrue(html.contains("<li><b>Combat</b>: 91</li>"));
+        Assert.assertTrue(html.contains("Safe &lt;tag&gt;"));
+    }
+}
+
